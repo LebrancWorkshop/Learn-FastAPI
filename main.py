e03c863902eaaed4470be20755bcea082e6e4b33
lebrancconvas <lebrancconvas@gmail.com>
2023-06-05 11:22:31 +0700

    ✨ feat: update path parameter with type

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/")
-def index():
+async def index():
   return {"message": "Hello, API"}
 
 @app.get("/ping")
-def ping():
+async def ping():
   return {"message": "pong"}
 
+# Path Parameter
+@app.get("/tutorial/path/{tutorial_name}")
+async def get_podcast(tutorial_name):
+  return {"tutorial_name": tutorial_name}
 
+# Path Parameter with Type
+@app.get("/tutorial/path/type/{tutorial_id}")
+async def get_podcast(tutorial_id: int): # Must use async
+  return {"tutorial_id": tutorial_id}
